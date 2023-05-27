1200d7f46f2ff2c6eaaa0cfd36fce145771462b8
pocoochan <saya.matsui@zozo.com>
2023-05-27 23:09:31 +0900

    init

---
+++ SampleCollectionView/SampleCollectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleCollectionView
+//
+//  Created by Saya Matsui on 2023/05/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SampleCollectionView/SampleCollectionViewUITests/SampleCollectionViewUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SampleCollectionViewUITestsLaunchTests.swift
+//  SampleCollectionViewUITests
+//
+//  Created by Saya Matsui on 2023/05/27.
+//
+
+import XCTest
+
+final class SampleCollectionViewUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
